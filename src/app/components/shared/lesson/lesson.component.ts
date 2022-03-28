2b2fc919b390522d606968a41ca0ad3a467a9333
aharonTamal <aharon@tamal.co.il>
2022-03-28 21:43:41 +0300

    add mitvot

--- src/app/components/shared/lesson/lesson.component.ts
+++ src/app/components/shared/lesson/lesson.component.ts
@@ -8,11 +8,25 @@ import {Lesson} from "../../../interfaces/lessons-interfaces";
 })
 export class LessonComponent implements OnInit {
 
-  @Input() lesson!: Lesson;
+  private _lesson!: Lesson;
+
+  @Input() set lesson(lesson: Lesson) {
+    this._lesson = lesson;
+    this.isMp3 = lesson.url.includes('mp3');
+  }
+
+  get lesson(): Lesson {
+    return this._lesson;
+  }
+
+  isMp3 = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onContextmenu() {
+    return false;
+  }
 }
